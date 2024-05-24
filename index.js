ab5c8a8d77000726268f2cbc214f5dafda18184e
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-05-24 20:40:04 +0700

    3. Đổi trạng thái một công việc

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const env = require("dotenv")
+const env = require("dotenv");
 env.config();
+const bodyParser = require('body-parser');
 const database = require("./config/database");
 database.connect();
 const app = express();
 const port = process.env.PORT;
+
+// parse application/json
+app.use(bodyParser.json());
 
 const routesApiV1 = require("./v1/routes/index.route");
 
--- v1/controllers/task.controller.js
+++ v1/controllers/task.controller.js
@@ -62,3 +62,28 @@ module.exports.detail = async (req, res) => {
 
   res.json(task);
 };
+
+// [GET] /api/v1/tasks/change-status/:id
+module.exports.changeStatus = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const status = req.body.status;
+
+    await Task.updateOne({
+      _id: id
+    }, {
+      status: status
+    });
+
+    res.json({
+      code: 200,
+      message: "Cập nhật trạng thái thành công!"
+    });
+  } catch (error) {
+    // console.log(error);
+    res.json({
+      code: 400,
+      message: "Không tồn tại bản ghi!"
+    });
+  }
+};
