ee45fb8a4209f21cf4656373214149b9f7de63b2
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-05-27 20:55:00 +0700

    2.1. Màn hình nhập email lấy mã OTP

--- v1/controllers/user.controller.js
+++ v1/controllers/user.controller.js
@@ -1,7 +1,9 @@
 const md5 = require("md5");
 const User = require("../models/user.model");
+const ForgotPassword = require("../models/forgot-password.model");
 
-const generate = require("../../helpers/generate.helper");
+const generateHelper = require("../../helpers/generate.helper");
+const sendEmailHelper = require("../../helpers/sendEmail.helper");
 
 // [POST] /api/v1/users/register
 module.exports.register = async (req, res) => {
@@ -22,7 +24,7 @@ module.exports.register = async (req, res) => {
     fullName: req.body.fullName,
     email: req.body.email,
     password: md5(req.body.password),
-    token: generate.generateRandomString(30),
+    token: generateHelper.generateRandomString(30),
   };
 
   const user = new User(dataUser);
@@ -69,5 +71,45 @@ module.exports.login = async (req, res) => {
     code: 200,
     message: "Đăng nhập thành công!",
     token: token
+  });
+};
+
+// [POST] /api/v1/users/password/forgot
+module.exports.passwordForgot = async (req, res) => {
+  const email = req.body.email;
+
+  const existUser = await User.findOne({
+    email: email,
+    deleted: false
+  });
+
+  if(!existUser) {
+    res.json({
+      code: 400,
+      message: "Email không tồn tại!"
+    });
+    return;
+  }
+
+  const otp = generateHelper.generateRandomNumber(6);
+
+  // Việc 1: Lưu email, OTP vào database
+  const objectForgotPassword = {
+    email: email,
+    otp: otp,
+    expireAt: Date.now() + 3*60*1000,
+  };
+
+  const forgotPassword = new ForgotPassword(objectForgotPassword);
+  await forgotPassword.save();
+
+  // Việc 2: Gửi mã OTP qua mail cho người dùng
+  const subject = "Lấy lại mật khẩu.";
+  const text = `Mã OTP xác thực tài khoản của bạn là: ${otp}. Mã OTP có hiệu lực trong vòng 3 phút. Vui lòng không cung cấp mã OTP này với bất kỳ ai.`;
+  sendEmailHelper.sendEmail(email, subject, text);
+
+  res.json({
+    code: 200,
+    message: "Đã gửi mã OTP qua email!"
   });
 };
